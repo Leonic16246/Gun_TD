6649124efb28fae27963328d38033d280c714e25
Leonic16246 <108039973+Leonic16246@users.noreply.github.com>
2023-10-20 01:15:07 +1300

    refactored scenes to be more modular

---
+++ Difficulty.cs
@@ -0,0 +1,46 @@
+using Godot;
+using System;
+
+public partial class Difficulty : Node3D
+{
+	[Export] public PackedScene enemy1Scene;
+	[Export] public PackedScene enemy2Scene;
+
+	public PathFollow3D spawnLocation;
+
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	public void GetSpawnLocation(PathFollow3D spawn)
+	{
+		spawnLocation = spawn;
+		GD.Print("spawn yes");
+	}
+
+	public void _on_timer_timeout()
+	{
+		// Create a new instance of the Mob scene.
+		Enemy enemy1 = enemy1Scene.Instantiate<Enemy>();
+		Enemy enemy2 = enemy2Scene.Instantiate<Enemy>();
+		// Choose a random location on the SpawnPath.
+		// We store the reference to the SpawnLocation node.
+
+		Vector3 playerPosition = GetParent().GetNode<Player>("Player").Position;
+		// And give it a random offset.
+		spawnLocation.ProgressRatio = GD.Randf();
+		enemy1.Initialize(spawnLocation.Position, playerPosition);
+
+		
+		enemy2.Initialize(spawnLocation.Position, playerPosition);
+
+		// Spawn the mob by adding it to the Main scene.
+		AddChild(enemy1);
+		GD.Print("enemy spawned");
+
+		AddChild(enemy2);
+		GD.Print("minion spawned");
+	}
+}
--- Main.cs
+++ Main.cs
@@ -5,12 +5,19 @@ public partial class Main : Node3D
 {
 	[Export] private PanelContainer mainMenu;
 	[Export] private PanelContainer MapMenu;
+	[Export] private PackedScene map1Scene;
+	[Export] private PackedScene map2Scene;
 
+	[Export] private PackedScene easyScene;
 	private bool mainMenuUp = true;
+
+	Game game;
+	Map map;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		game = (Game)GetParent();
 	}
 
 	public void _on_new_button_pressed()
@@ -22,16 +29,17 @@ public partial class Main : Node3D
 	}
 	public void _on_map_1_button_pressed()
 	{
-		GetTree().ChangeSceneToFile("res://Game.tscn");
+		game.InitializeGame(map1Scene, easyScene);
+		QueueFree();
 	}
 	public void _on_map_2_button_pressed()
 	{
-		GetTree().ChangeSceneToFile("res://Game.tscn");
+		game.InitializeGame(map2Scene, easyScene);
+		QueueFree();
 	}
 	public void _on_back_button_pressed()
 	{
 		MapMenu.Hide();
-
 		mainMenu.Show();
 		mainMenuUp = true;
 	}
